cd6bc686884a9ca5c6032b664afd7aa0647b0798
s0c5 <david.barinas.dev@gmail.com>
2015-03-05 13:15:15 -0500

    [+] Add Examples for use validators parameters and route anabeljs

---
+++ example/models/user.js
@@ -0,0 +1,34 @@
+/**
+ * Created by s0c45 on 5/03/15.
+ */
+
+var anabel      = require('anabeljs');
+var mongoose    = require('mongoose');
+
+
+
+var userSchema = new mongoose.Schema({
+    cedula: { 
+        type: Number,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['d','a']
+    },
+    name: {
+        type: String,
+        default: 'Anonymous',
+        validator: /\w+/
+    },
+    password: {
+        required: true,
+        type: String,
+        validator: /^.{10,100}$/
+    }
+});
+
+
+module.exports = mongoose.model('user', userSchema);
+
+
--- example/simple.js
+++ example/simple.js
@@ -1,4 +1,8 @@
 var anabel  = require('../');
+var morgan  = require('morgan');
+var bodyParser = require('body-parser');
+
+
 
 var app = anabel.init({
     dirName: __dirname,
@@ -7,22 +11,108 @@ var app = anabel.init({
     modelPath: './models'
 });
 
+var sum = app.lib('sum');
+var sub = app.lib('sub');
+
+app.middleware(bodyParser.json());
+app.middleware(bodyParser.urlencoded({extended: true}));
+app.middleware(morgan('dev'));
+
+
 
 app.get('/', function(req, res, next){
    res.json({message: "Anabel JS"});
 });
 
 app.get('/sum', function(req, res, next){
-    var sum = app.lib('sum');
     res.json({result: sum(2,2) })
 });
 
 app.get('/sub', function(req, res, next){
-    var sub = app.lib('sub');
+
     res.json({result: sub(5, 3) });
 });
 
+var Books = {};
+var Users  = {};
 
-app.listen(8888,function(){
+var book = function(){
+    
+    this.create = function(req, res, next){
+        var book    = req.body;
+        Books[book.serial] = book;
+        res.json(book);
+    };
+    console.log(anabel.validator);
+    this.create.input = [
+        {
+            field: 'body',
+            schema: {
+                serial: {
+                    type: Number,
+                    required: true,
+                    format: function(x){ return x>100}
+                },
+                tittle: String,
+                author: {
+                    type: String
+                }
+            }
+        }
+    ];
+    this.create.output = {
+        serial: Number,
+        tittle: String,
+        author: String
+    };
+};
+
+var bookCRUD = new book();
+
+
+var userModel = anabel.model('user');
+
+var user = function(){
+
+    this.create = function(req, res, next){
+        var user    = req.body;
+        Users[user.cedula] = user;
+        res.json(user);
+    };
+    
+    this.create.input = [
+        {
+            field: 'body',
+            schema: userModel
+        }
+    ];
+    this.create.output = {
+        serial: Number,
+        tittle: String,
+        author: String
+    };
+};
+
+var userCRUD = new user();
+anabel.route([
+    {
+        method: 'POST',
+        path: '/book',
+        description: 'create a new book',
+        controller: bookCRUD.create,
+        input: bookCRUD.create.input,
+        output: bookCRUD.create.output
+    },
+    {
+        method: 'POST',
+        path: '/user',
+        description: 'create a new user',
+        controller: userCRUD.create,
+        input: userCRUD.create.input,
+        output: userCRUD.create.output
+    }
+]);
+
+app.listen(8889,function(){
     console.info("listen in 8888" )
 });
